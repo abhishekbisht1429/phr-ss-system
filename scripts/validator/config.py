c0ac303203d5767e5c0f3efce3c842ec748a3260
Abhishek Bisht <abhishekbisht1429@gmail.com>
2022-10-10 04:07:53 +0530

    removed unnecessary code from config.py

--- scripts/validator/config.py
+++ scripts/validator/config.py
@@ -11,27 +11,8 @@ user_key_dir = _path['user']['key_dir']
 
 
 _address = config['address']
-server_ip = _address['server']['ip']
-server_port = _address['server']['port']
-
 kds_ip = _address['kds']['ip']
 kds_port = _address['kds']['port']
-
-# network_ip = _address['network']['ip']
-# network_port = _address['network']['port']
-#
-# component_ip = _address['component']['ip']
-# component_port = _address['component']['port']
-#
-# consensus_ip = _address['consensus']['ip']
-# consensus_port = _address['consensus']['port']
-
-# network_endpoint = 'network:tcp://'+config.network_ip+':'+\
-#                    str(config.network_port)
-# component_endpoint = 'component:tcp://'+config.component_ip+':'\
-#                      + str(config.component_port)
-# consensus_endpoint = 'consensus:tcp://'+config.consensus_ip+':'+\
-#                      str(config.consensus_port)
 
 
 _key = config['key']
